f63b089779076a52bc5b91cfb68fcfda038dd9b9
HYEONJISOO <hyeon.jisoo25@gmail.com>
2022-12-23 14:24:29 +0900

    app_name n post redirect reverse

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, render
 from accountapp.models import HelloWorld
+from django.urls import reverse
+
 # Create your views here.
 
 
@@ -15,8 +17,12 @@ def hhh(request):
         new_hhh = HelloWorld()       # hhh라는 모델(빵틀ㅋㅋㅋ)에서 나온 새로운 객체가 저장
         new_hhh.text = temp          # 템프에 받았던 데이터를 이 새로운 객체의 text 에 넣음
         new_hhh.save()               # DB에 저장
+        
+        hhh_list = HelloWorld.objects.all()
 
-
-        return render(request, 'accountapp/hhh.html', context={'hhh_output' : new_hhh }) # 객체를 내보냄
+        # return render(request, 'accountapp/hhh.html', context={'hhh_list' : hhh_list }) # 객체를 내보냄
+        # 렌더로 하면 새로고침할때 계속 포스트하는 문제 발생
+        return HttpResponseRedirect(reverse('accountapp:hhhu'))
     else:
-        return render(request, 'accountapp/hhh.html', context={'hhh_output' : "GET METHOD!!"})
+        hhh_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hhh.html', context={'hhh_list' : hhh_list })
