cad1503fc2d8878b60955b58c728df8b6dacd05c
HYEONJISOO <hyeon.jisoo25@gmail.com>
2022-12-23 11:19:29 +0900

    migration

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+# models 의 Models 가져옴
+# 거기서 추가적인 정보입력 해서 그걸 클래스로 만듦
+class hhh(models.Model):
+    text = models.CharField(max_length=255, null = False)
