77295c875541d66aab0ca1c356a33fb186072e99
HYEONJISOO <hyeon.jisoo25@gmail.com>
2022-12-29 22:29:03 +0900

    29. mediaurl n root

--- accountapp/views.py
+++ accountapp/views.py
@@ -21,35 +21,8 @@ from django.utils.decorators import method_decorator
 
 app_name = 'accountapp' 
 
-# def hhh(request):
- 
-#     if request.user.is_authenticated:      # 유저가 로그인했는지 확인
-
-
-#         # post get 구분!
-#         if request.method == "POST":
-#             temp = request.POST.get("hhh_input")
-#             # 리퀘스트에서 포스트에서 hhh.input 이라는 이름을 가진 데이터를 가져와라
-#             new_hhh = HelloWorld()       # hhh라는 모델(빵틀ㅋㅋㅋ)에서 나온 새로운 객체가 저장
-#             new_hhh.text = temp          # 템프에 받았던 데이터를 이 새로운 객체의 text 에 넣음
-#             new_hhh.save()               # DB에 저장
-#             # 데이터 저장
-            
-
-#             # return render(request, 'accountapp/hhh.html', context={'hhh_list' : hhh_list }) # 객체를 내보냄
-#             # 렌더로 하면 새로고침할때 계속 포스트하는 문제 발생
-#             return HttpResponseRedirect(reverse('accountapp:hhhu'))
-#         else:
-#             hhh_list = HelloWorld.objects.all()
-#             return render(request, 'accountapp/hhh.html', context={'hhh_list' : hhh_list })
-
-#     else:
-#         return HttpResponseRedirect(reverse('accountapp:login'))
-
-has_owndership = [account_ownership_required, login_required]
 ### decrator 사용해서 코드 깔끔하게 다듬기 
-
-
+has_owndership = [account_ownership_required, login_required]
 
 @login_required         # 방금 적었었던 로그인했는지, 아니면 로그인 페이지로 리턴하는 것까지 여기 담겨있음
 def hhh(request):
@@ -95,7 +68,7 @@ class AccountDetailView(DetailView):
 
 
 # 여기서 겟은 클래스 안에있는 함수 = 메서드 이기 때문에 그냥 @login_required 써도 작동안됨 
-@method_decorator(login_required, 'get') # 일반 펑션에 사용하는 데코레이터를 메서드에 사용할 수 있게 만들어주는 데코레이터 ㅋㅋㅋㅋ
+@method_decorator(login_required, 'get')        # 일반 펑션에 사용하는 데코레이터를 메서드에 사용할 수 있게 만들어주는 데코레이터 ㅋㅋㅋㅋ
 @method_decorator(login_required, 'post') 
 @method_decorator(account_ownership_required, 'get') 
 @method_decorator(account_ownership_required, 'post') 
@@ -105,6 +78,19 @@ class AccountUpdateView(UpdateView):
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hhhu')
     template_name = 'accountapp/update.html'
+
+
+@method_decorator(has_owndership, 'get')      # 리스트에 넣어줘서 한꺼번에 데코레이터 사용할 수 있게만들어줌!
+@method_decorator(has_owndership, 'post') 
+class AccountDeleteView(DeleteView):
+    model = User
+    context_object_name = "target_user"
+
+    success_url = "accountapp:login"
+    template_name = "accountapp/delete.html"
+
+
+
 
 # 필요없음!
     # def get(self, *args, **kwargs):
@@ -124,14 +110,27 @@ class AccountUpdateView(UpdateView):
 # @method_decorator(login_required, 'post') 
 # @method_decorator(account_ownership_required, 'get') 
 # @method_decorator(account_ownership_required, 'post') 
-@method_decorator(has_owndership, 'get') 
-@method_decorator(has_owndership, 'post') 
-
-class AccountDeleteView(DeleteView):
-    model = User
-    context_object_name = "target_user"
-
-    success_url = "accountapp:login"
-    template_name = "accountapp/delete.html"
 
 
+
+# def hhh(request):
+#     if request.user.is_authenticated:      # 유저가 로그인했는지 확인
+#         # post get 구분!
+#         if request.method == "POST":
+#             temp = request.POST.get("hhh_input")
+#             # 리퀘스트에서 포스트에서 hhh.input 이라는 이름을 가진 데이터를 가져와라
+#             new_hhh = HelloWorld()       # hhh라는 모델(빵틀ㅋㅋㅋ)에서 나온 새로운 객체가 저장
+#             new_hhh.text = temp          # 템프에 받았던 데이터를 이 새로운 객체의 text 에 넣음
+#             new_hhh.save()               # DB에 저장
+#             # 데이터 저장
+            
+
+#             # return render(request, 'accountapp/hhh.html', context={'hhh_list' : hhh_list }) # 객체를 내보냄
+#             # 렌더로 하면 새로고침할때 계속 포스트하는 문제 발생
+#             return HttpResponseRedirect(reverse('accountapp:hhhu'))
+#         else:
+#             hhh_list = HelloWorld.objects.all()
+#             return render(request, 'accountapp/hhh.html', context={'hhh_list' : hhh_list })
+
+#     else:
+#         return HttpResponseRedirect(reverse('accountapp:login'))
